ac237a4ad703b41908f8211c2b046a9dbbc02f01
Gautam Singh Mahara <107118978+jennie0612@users.noreply.github.com>
2023-05-20 21:27:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask,jsonify,request
+import jwt
+import os
+from PIL import Image
+from pytesseract import pytesseract
+
+app = Flask(__name__)
+
+@app.route('/',methods=["POST"])
+def imageProcess():
+    try:
+        path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        image_file = request.files['image']
+        temp_image_path = 'temp_image.png'
+        image_file.save(temp_image_path)
+        img = Image.open(temp_image_path)
+        pytesseract.tesseract_cmd = path_to_tesseract
+
+        text = pytesseract.image_to_string(img)
+        os.remove(temp_image_path)
+        return jsonify({"message":text})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+        
+        
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
