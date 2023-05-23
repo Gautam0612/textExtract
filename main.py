1699a29d4b0bb74dd2518d5b42d356aeb446c06e
Gautam Singh Mahara <107118978+jennie0612@users.noreply.github.com>
2023-05-23 12:33:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from flask import Flask,jsonify,request
 import os
 from PIL import Image
 from pytesseract import pytesseract
-
 app = Flask(__name__)
 
-@app.route('/image',methods=["POST"])
+@app.route('/',methods=["POST"])
 def imageProcess():
     try:
-        path_to_tesseract = "Tesseract-OCR/tesseract.exe"
         image_file = request.files['image']
         temp_image_path = 'temp_image.png'
         image_file.save(temp_image_path)
         img = Image.open(temp_image_path)
-        pytesseract.tesseract_cmd = path_to_tesseract
-
         text = pytesseract.image_to_string(img)
         os.remove(temp_image_path)
         return jsonify({"message":text})
